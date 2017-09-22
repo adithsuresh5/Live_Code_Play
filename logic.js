019442639c0e2ef876edda3cbbb6f0f458650b96
Shubham Vyas <vyashubhamkumar@gmail.com>
2017-09-22 15:01:23 +0530

    added Default Values to HTML and CSS Container
    To show Live functionality

--- logic.js
+++ logic.js
@@ -14,6 +14,24 @@ $(document).ready( function(){
 			var navBarHeight = $(".navbar").height();
 			var codeContainerHeight = windowHeight - navBarHeight;
 
+			var defaultHTMLInput = "<div class='loader'></div>";
+			var defaultCSSInput =  ".loader{"+
+    								"\n\tborder: 16px solid #f3f3f3; /* Light grey */"+
+    								"\n\tborder-top: 16px solid #3498db; /* Blue */ "+
+    								"\n\tborder-radius: 50%;"+
+    								"\n\twidth: 120px;"+
+								    "\n\theight: 120px;"+
+								    "\n\tanimation: spin 2s linear infinite;"+
+								    "\n\tmargin:0 auto;"+
+								    "\n\tmargin-top:55px;"+
+								"\n}"+
+								"\n@keyframes spin {"+
+								    "\n\t0% { transform: rotate(0deg); }"+
+								    "\n\t100% { transform: rotate(360deg);"+
+								    "}"+
+								    "\n }";
+
+
 			$(".code-container").height(codeContainerHeight+"px");
 
 			$(".code-container").css("marginTop",navBarHeight+1);
@@ -64,6 +82,15 @@ $(document).ready( function(){
 	var p = $("#resultcontainer").contents().find("body");
 	p.css("margin","2px");
  	p.html('<span id="bothtml"></span><style id="botcss"></style>');
+
+ 	// Default Values:
+
+ 	$("#htmlcontainer").html(defaultHTMLInput);
+ 	$("#csscontainer").html(defaultCSSInput);
+ 	p.find("#bothtml").html(defaultHTMLInput);
+ 	p.find("#botcss").html(defaultCSSInput);
+
+
  	
  	$("#htmlcontainer").on('keyup',function(){
   		p.find("#bothtml").html($(this).val());
